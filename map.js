95861a8ed39d3d0adcf65b3201ebe5a889b60c82
Emerson1227 <shg32@txstate.edu>
2025-10-15 22:53:56 -0500

    Emersons Branch

--- map.js
+++ map.js
@@ -1,8 +1,27 @@
-var map = L.map('map').setView([29.8884, -97.9384], 14);
-mapLink =
+
+  var map = L.map('map').setView([29.8884, -97.9384], 14);
+
+var mapLink =
     '<a href="http://openstreetmap.org">OpenStreetMap</a>';
 L.tileLayer(
     'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; ' + mapLink + ' Contributors',
     maxZoom: 18,
     }).addTo(map);
+    
+var polygon = turf.polygon([[
+  [-97.94, 29.885],
+  [-97.935, 29.885],
+  [-97.935, 29.890],
+  [-97.94, 29.890], 
+  [-97.94, 29.885]  
+]]);
+
+L.geoJSON(polygon, {
+  style: {
+    color: 'green', 
+    weight: ,
+    fillColor: 'lightgreen',
+    fillOpacity: 0.5
+  }
+}).addTo(map);
